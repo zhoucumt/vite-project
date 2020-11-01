8ad73df262b9187fc3dfbe0d4d30e7e1fbef12e2
zhoucumt <553817478@qq.com>
2020-11-01 13:47:01 +0800

    暂时去掉自定义渲染器

--- src/components/Video.vue
+++ src/components/Video.vue
@@ -1,11 +1,33 @@
 <template>
   <div>
-    <video controls width="640" height="380"></video>
+    <video controls width="640" height="380" src=""></video>
   </div>
 </template>
 
 <script>
+import videoSrc from '../assets/test.mp4';
+
 export default {
-  name: 'Barrage'
+  name: 'Video',
+  setup () {
+    const state = reactive({
+      data: [
+        {name: '大专', count: 200, color: 'brown'},
+        {name: '本科', count: 300, color: 'yellow'},
+        {name: '硕士', count: 100, color: 'pink'},
+        {name: '博士', count: 50, color: 'skyblue'}
+      ],
+      src: videoSrc
+    })
+
+    function handleClick() {
+      state.data.push({name: '其他', count: 30, color: 'orange'},)
+    }
+
+    return {
+      state,
+      handleClick
+    }
+  },
 }
 </script>
